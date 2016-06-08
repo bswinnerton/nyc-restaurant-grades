831ac7154d1c30b1f314a2a8b75eb0480d70d949
Brooks Swinnerton <bswinnerton@gmail.com>
2016-06-08 10:50:58 -0400

    Document Inspection type

--- app/graph/graph/types/inspection.rb
+++ app/graph/graph/types/inspection.rb
@@ -7,15 +7,17 @@ module Graph
       interfaces [NodeIdentification.interface]
       global_id_field :id
 
-      field :type,                    types.String
-      field :violationDescription,    types.String,   property: :violation_description
-      field :violationCode,           types.String,   property: :violation_code
-      field :grade,                   types.String
-      field :score,                   types.Int
-      field :inspectedAt,             !types.String,  property: :inspected_at
-      field :gradedAt,                types.String,   property: :graded_at
+      field :type,                 types.String,  'The type of inspection.'
+      field :violationDescription, types.String,  'The description of the violation.', property: :violation_description
+      field :violationCode,        types.String,  'The violation code cited.', property: :violation_code
+      field :grade,                types.String,  'The grade received.'
+      field :score,                types.Int,     'The numeric score received.'
+      field :inspectedAt,          !types.String, 'The timestamp of the inspection.', property: :inspected_at
+      field :gradedAt,             types.String,  'The timestamp of when the grade was received.',  property: :graded_at
 
       field :restaurant, -> { !Types::Restaurant } do
+        description 'The restaurant associated with the inspection.'
+
         resolve -> (object, arguments, context) do
           object.restaurant
         end
