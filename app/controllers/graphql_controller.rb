8dca3d5f1c89e767b1e55be158f443bf6575e927
Brooks Swinnerton <bswinnerton@gmail.com>
2017-03-12 15:43:53 -0400

    Don't require authenticity token for endpoint

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,4 +1,6 @@
 class GraphqlController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def create
     result = Graph::Schema.execute(params[:query], variables: params[:variables])
     render json: result
