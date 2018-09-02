e85fdfa03af521f85c0ae5c4fdcfac63c5e5f10a
Brooks Swinnerton <bswinnerton@gmail.com>
2018-09-02 10:19:06 -0400

    Revert "Use nested relationships in activerecord-import"
    This reverts commit c20b44f862ffba29a53e92fb4a943c288f53e484.

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,10 @@ end
 
 broken = false
 dataset = DataSet.new
-restaurants = {}
+
+restaurants = []
+inspections = []
+violations  = []
 
 Rails.logger.info "Starting import of NYC OpenData dataset..."
 
@@ -48,47 +51,61 @@ while !broken
 
     parsed_response.each do |restaurant_data|
       next unless restaurant_data['dba']
-      camis = restaurant_data['camis']
 
-      if !restaurants[camis]
-        restaurants[camis] = Restaurant.new(
-          camis:            camis,
-          name:             restaurant_data['dba'].titleize,
-          building_number:  restaurant_data['building'],
-          street:           restaurant_data['street'].titleize,
-          zipcode:          restaurant_data['zipcode'],
-          borough:          Restaurant.boroughs[restaurant_data['boro'].parameterize.underscore.upcase],
-          phone_number:     restaurant_data['phone'],
-          cuisine:          restaurant_data['cuisine_description'],
+      restaurant = Restaurant.new(
+        camis:            restaurant_data['camis'],
+        name:             restaurant_data['dba'].titleize,
+        building_number:  restaurant_data['building'],
+        street:           restaurant_data['street'].titleize,
+        zipcode:          restaurant_data['zipcode'],
+        borough:          Restaurant.boroughs[restaurant_data['boro'].parameterize.underscore.upcase],
+        phone_number:     restaurant_data['phone'],
+        cuisine:          restaurant_data['cuisine_description'],
+      )
+
+      restaurants << restaurant
+    end
+
+    parsed_response.each do |restaurant_data|
+      next unless restaurant_data['dba']
+
+      inspection = Inspection.new(
+        inspected_at: restaurant_data['inspection_date'].to_datetime,
+        type:         restaurant_data['inspection_type'],
+        graded_at:    restaurant_data['grade_date'],
+        score:        restaurant_data['score'],
+        grade:        restaurant_data['grade'],
+      )
+
+      inspection.restaurant_camis = restaurant_data['camis']
+
+      inspections << inspection
+    end
+
+    parsed_response.each do |restaurant_data|
+      next unless restaurant_data['dba']
+
+      violation_description = restaurant_data['violation_description']
+      violation_code        = restaurant_data['violation_code']
+
+      if violation_description || violation_code
+        violation = Violation.new(
+          code:         violation_code,
+          description:  violation_description,
         )
-      else
-        restaurant = restaurants[camis]
 
-        restaurant.inspections << Inspection.new(
-          inspected_at: restaurant_data['inspection_date'].to_datetime,
-          type:         restaurant_data['inspection_type'],
-          graded_at:    restaurant_data['grade_date'],
-          score:        restaurant_data['score'],
-          grade:        restaurant_data['grade'],
-        )
+        violation.restaurant_camis  = restaurant_data['camis']
+        violation.inspected_at      = restaurant_data['inspection_date'].to_datetime
 
-        violation_description = restaurant_data['violation_description']
-        violation_code        = restaurant_data['violation_code']
-
-        if violation_description || violation_code
-          inspection = restaurant.inspections.find do |inspection|
-            inspection.inspected_at == restaurant_data['inspection_date'].to_datetime
-          end
-
-          inspection.violations << Violation.new(
-            code:         violation_code,
-            description:  violation_description,
-          )
-        end
+        violations << violation
       end
     end
 
     dataset.page += 1
+
+    restaurants = restaurants.uniq
+    inspections = inspections.uniq
+    violations  = violations.uniq
   rescue Exception => exception
     Rails.logger.info "Fetching failed at page #{dataset.page}."
     Rails.logger.fatal exception
@@ -97,7 +114,39 @@ while !broken
 end
 
 Rails.logger.info "Importing Restaurants..."
-Restaurant.import(restaurants.values, batch_size: 10000, on_duplicate_key_ignore: { conflict_target: [:camis], columns: [:updated_at] })
+Restaurant.import(restaurants, batch_size: 50000, on_duplicate_key_ignore: { conflict_target: [:camis], columns: [:updated_at] })
+
+Rails.logger.info "Caching Restaurants..."
+persisted_restaurants = Restaurant.all.group_by(&:camis)
+
+Rails.logger.info "Building Inspections..."
+inspections = inspections.map do |inspection|
+  restaurant = persisted_restaurants[inspection.restaurant_camis].first
+  inspection.restaurant_id = restaurant.id
+  inspection
+end
+
+Rails.logger.info "Importing Inspections..."
+Inspection.import(inspections, batch_size: 50000, on_duplicate_key_ignore: true)
+
+Rails.logger.info "Caching Inspections..."
+persisted_inspections = Inspection.all.group_by(&:restaurant_id)
+
+Rails.logger.info "Building Violations..."
+violations = violations.map do |violation|
+  restaurant = persisted_restaurants[violation.restaurant_camis].first
+  restaurant_inspections = persisted_inspections[restaurant.id]
+
+  inspection = restaurant_inspections.find do |inspection|
+    inspection.inspected_at == violation.inspected_at
+  end
+
+  violation.inspection = inspection
+  violation
+end
+
+Rails.logger.info "Importing Violations..."
+Violation.import(violations, batch_size: 50000, on_duplicate_key_ignore: true)
 
 Rails.logger.info "Import of NYC OpenData dataset complete."
 
